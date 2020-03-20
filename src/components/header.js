2af1373df187bd1a0bfa48be4b76f2b82ae606d0
Santiago M <smquintero99@gmail.com>
2020-03-20 12:17:47 -0700

    Initial Map Commit

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,7 @@ import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    style={{ background: `rebeccapurple`, height: '10vh' }}
   >
     <div
       style={{
