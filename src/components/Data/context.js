838f4ef6e4b490f100fc27b734d0874b0a722645
Santiago M <smquintero99@gmail.com>
2020-03-21 13:27:37 -0700

    Connect to Mongo, Set up DB Provider & First POST Query!

---
+++ src/components/Data/context.js
@@ -0,0 +1,35 @@
+import { RemoteMongoClient, Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+import React, { useState, useEffect } from "react"
+
+
+const client = Stitch.initializeDefaultAppClient("geo-protection-vaqca")
+// client.auth.loginWithCredential(new AnonymousCredential())
+
+const mongo = client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas")
+const db = mongo.db("geodatadev").collection('healthmap')
+
+const get_docs = () => db.find({}, { limit: 100}).asArray().catch(console.log)
+export const { Provider, Consumer } = React.createContext()
+
+
+
+export const DBProvider = ({ children }) => {
+    const [ id, setId ] = useState(null)
+    const [ docs, setDocs ] = useState([])
+  
+    useEffect(() => {
+      async function fetchData(){
+        const { id } = await client.auth.loginWithCredential(new AnonymousCredential())
+        setId(id)
+
+        const docs = await get_docs()
+        setDocs(docs)
+        console.log(docs, id)
+      }
+      
+      fetchData()
+    }, [])
+
+    return <Provider value={{docs: docs, db: db, id: id}}>{children}</Provider>
+}
+
--- src/components/Forms/WelcomeForm.js
+++ src/components/Forms/WelcomeForm.js
@@ -1,34 +1,51 @@
+import { Consumer } from '../Data/context'
 import React, { useState } from 'react'
 
 
 const BUTTON_STYLE = {width:200, margin:'1rem'}
 export const WelcomeForm = () => {
     const [ isActive, setActive ] = useState('is-active')
-    const post = value => {
-        console.log(value)
+    const post = async (doc, db, id) => {
+        await db.insertOne({value: doc, owner_id:id}).catch(console.log)
+        const docs = await db.find({}, { limit: 100 }).asArray().catch(console.log)
+        console.log(docs)
         setActive('')
     }
 
-    return <div className={`modal ${isActive}`}>
-        <div className='modal-background'/>
-        <div className='modal-content' style={{width:350}}>
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title">  How do you feel? </p>
-                    <a href="#" class="card-header-icon" aria-label="more options">
-                    <span class="icon">
-                        <i class="fas fa-angle-down" aria-hidden="true"></i>
-                    </span>
-                    </a>
-                </header>
-                <div class="card-content">
-                    <div class="content has-text-centered">
-                        <button class="button is-success is-light" style={BUTTON_STYLE} onClick={() => post(0)}>I'm Fine</button><br/>
-                        <button class="button is-warning is-light" style={BUTTON_STYLE} onClick={() => post(1)}>I have symptoms</button><br/>
-                        <button class="button is-danger is-light" style={BUTTON_STYLE} onClick={() => post(2)}>Definitively Sick</button>
+    return <Consumer>{({ db, id }) => 
+        <div className={`modal ${isActive}`}>
+            <div className='modal-background'/>
+            <div className='modal-content' style={{width:350}}>
+                <div className="card">
+                    <header className="card-header">
+                        <p className="card-header-title">  How do you feel? </p>
+                        <a href="#" className="card-header-icon" aria-label="more options">
+                        <span className="icon">
+                            <i className="fas fa-angle-down" aria-hidden="true"></i>
+                        </span>
+                        </a>
+                    </header>
+                    <div className="card-content">
+                        <div className="content has-text-centered">
+                            <button 
+                                className="button is-success is-light" 
+                                style={BUTTON_STYLE} 
+                                onClick={() => post(0, db, id)}
+                            >I'm Fine</button><br/>
+                            <button 
+                                className="button is-warning is-light" 
+                                style={BUTTON_STYLE} 
+                                onClick={() => post(1, db, id)}
+                            >I have symptoms</button><br/>
+                            <button 
+                                className="button is-danger is-light" 
+                                style={BUTTON_STYLE} 
+                                onClick={() => post(2, db, id)}
+                            >Definitively Sick</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    }</Consumer>
 }
