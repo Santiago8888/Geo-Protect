2f1f0220e622674d0de0aea2f5723d6fc297c64b
Santiago M <smquintero99@gmail.com>
2020-03-22 15:13:37 -0700

    Cities Layer, First Deployed Observatiopns & Stages.

--- src/data/queries.js
+++ src/data/queries.js
@@ -1,8 +1,18 @@
 export const countries_pipeline = [{
-      '$group': {
-        '_id': '$country', 
-        'total': { '$sum': 1 }, 
-        'sick': { '$sum': '$value' }, 
-        'coords': { '$first': '$coords' }
-      }
+	'$group': {
+		'_id': '$country', 
+		'total': { '$sum': 1 }, 
+		'sick': { '$sum': '$value' }, 
+		'coords': { '$first': '$coords' }
+	}
+}]
+
+
+export const cities_pipeline = [{
+	'$group': {
+		_id: "$city",
+		total: { $sum: 1 },
+		sick: { $sum: "$value" },
+		coords: { $first: "$coords"}
+  	}
 }]
